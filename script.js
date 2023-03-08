1d80780e7c6906679bc0e81df9408a260625d2cf
pr-melo <119916150+pr-melo@users.noreply.github.com>
2023-03-08 20:56:39 -0300

    Desafio pagina web

---
+++ script.js
@@ -0,0 +1,11 @@
+let preço = "1.000.000"
+let nome ="Ferrari"
+let logado = true
+let lista = ["ovo", "massa", "19.90"]
+let nomeCompleto ={nome:"Renato", idade: 90}
+let cidade = "São Paulo"
+
+console.log(nome)
+console.log(preço)
+console.log(lista)
+console.log(cidade)
